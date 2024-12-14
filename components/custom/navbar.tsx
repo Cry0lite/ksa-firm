affb8c2be711fc75d53ddccf48ea3e56eba9e4cd
Cry0lite <avishkargunjal008@gmail.com>
2024-12-14 22:36:06 +0530

    improved navigation

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -30,43 +30,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+
 
 export function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -90,7 +54,7 @@ export function Navbar() {
       )}
     >
       <div className="flex justify-between items-center py-6 md:p-6 w-5/6 lg:w-4/5">
-        <p className=" font-bold">KSA and Associates</p>
+        <a href="/" className=" font-bold">KSA and Associates</a>
         <NavigationMenu className="hidden md:block">
           <NavigationMenuList>
             {/* <NavigationMenuItem>
@@ -183,15 +147,26 @@ export function Navbar() {
             </SheetHeader>
             <div className="grid gap-4 py-4 mt-4">
               <ul className="flex flex-col justiy-center items-center gap-4">
-                <a className="transition-all" href="#about">
+              <SheetClose asChild>
+              <a className="transition-all" href="/">
+                  Home
+                </a>
+              </SheetClose>
+              <SheetClose asChild>
+              <a className="transition-all" href="/#about">
                   About
                 </a>
-                <a className="transition-all" href="#services">
+              </SheetClose>
+              <SheetClose asChild>
+              <a className="transition-all" href="/#services">
                   Services
                 </a>
-                <a className="transition-all" href="#contact">
+              </SheetClose>
+              <SheetClose asChild>
+              <a className="transition-all" href="#contact">
                   Contact Us
                 </a>
+              </SheetClose>
               </ul>
             </div>
             {/* <SheetFooter>
